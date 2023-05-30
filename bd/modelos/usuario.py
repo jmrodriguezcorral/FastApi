9924e86765925fb0ab5ef62d891a4f929794c0c4
jmrodriguezcorral <jmchema@gmail.com>
2023-05-30 19:01:19 +0200

    Despliegue para deta

--- bd/modelos/usuario.py
+++ bd/modelos/usuario.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
+# Metemos este tipo por compatibilidad con python 3.9 con nueva version |None
+from typing import Optional
 
 class User(BaseModel):
-    id: str | None
+    id: Optional[str]
     nombre_usuario: str
     correo: str
